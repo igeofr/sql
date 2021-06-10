8b4cbb7af949bfdc27744c986ec66a7263659f48
Flø <florian.boret@laposte.net>
2021-06-10 14:28:35 +0200

    Update tables_views_owner.sql

--- tables_views_owner.sql
+++ tables_views_owner.sql
@@ -3,11 +3,11 @@
 
 SELECT *
 FROM pg_views t
-WHERE t.viewowner != 'toto';
+WHERE t.viewowner = 'toto';
 /*--------------------------------------------------*/
 /*Tables*/
 
 SELECT *
 FROM pg_tables t
-WHERE t.tableowner != 'toto';
+WHERE t.tableowner = 'toto';
 /*--------------------------------------------------*/
